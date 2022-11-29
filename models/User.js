45726f4394a53123e4f0a0f11f70d6e1d0e43b27
Maggie Lively <maggielively@Maggie-Livelys-Pro.local>
2022-11-28 20:07:53 -0700

    first attempt at user.js model

--- models/User.js
+++ models/User.js
@@ -3,19 +3,32 @@ const { Schema, model} = require('mongoose');
 const userSchema = new Schema(
     {
         username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
 
         },
         email: {
+            type: String,
+            required: true,
+            unique: true,
+            //TODO: add match a valid email address
+            match: /.+\@.+\..+/,
 
         },
         thoughts: [
             {
                 // references the Thought ObjectId
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
             },
         ],
         friends : [
             {
                 // references the User ObjectId
+                type: Schema.Types.ObjectId,
+                ref: 'User',
             },
         ],
     },
@@ -27,7 +40,12 @@ const userSchema = new Schema(
     }
 );
 
-// virtual for friendCount - reference activities 21 & 23
+// virtual for friendCount that retrieves the length of the user's friends array field on query.- reference activities 21 & 23
+userSchema
+.virtual('friendCount')
+.get(function () {
+    return this.friends.length;
+});
 
 const User = model('User', userSchema);
 
